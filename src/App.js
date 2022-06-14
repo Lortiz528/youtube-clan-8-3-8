a5180184d76d86763a9d44bb6991205e74afdbb2
Stephanie Frias <steph.frias01@me.com>
2022-06-14 11:26:16 -0400

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React LEARN REACT TEST
+          Learn React LEARN REACT TEST test
         </a>
       </header>
     </div>
