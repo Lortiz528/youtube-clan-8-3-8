2a6a2164e301c64a9ea3d1bb0a3c9e8fad2ff972
named.josie <named.josie@gmail.com>
2022-06-14 11:24:58 -0400

    test 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-    
+      <Navbar />
         <h1>
           HELLO WORLD
         </h1>
