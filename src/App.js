cd3ebee0bcfde272c1ce757e930daf6b7ebac1c7
named.josie <named.josie@gmail.com>
2022-06-14 20:13:56 -0400

    added routes basic skeleton for app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-
 import './App.css';
 import Navbar from './components/common/Navbar';
+import About from './components/pages/about'
+import Home from './components/pages/home'
+import Header from './components/common/header'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <Navbar />
-        <h1>
-          HELLO WORLD
-        </h1>
-        
-      </header>
-    </div>
-  );
+    <Router>
+    <Header />
+    <div className="wrapper">
+    <Nav />
+    <main>
+    <Routes>
+     <Route path = "/" element={<Home/>} />
+     <Route path ='/about' element={<About/>} />
+     </Routes>
+      </main>
+  </div>
+  <Footer />
+  </Router>
+  </div>
+);
 }
 
 export default App;
