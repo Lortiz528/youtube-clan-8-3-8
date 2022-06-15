55f9e63501585d872f1ae4060ea249cb95bf4862
Lortiz528 <lortiztech@gmail.com>
2022-06-14 23:04:08 -0400

    component edit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,24 @@ import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import Searchbar from './components/Searchbar';
 import About from './components/About';
+import Test from './components/Test';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      videos: [],
-    };
+    this.state = {};
   }
 
   render() {
     return (
-      <div>
-        <p>Hello world(test)</p>
+      <div className="App">
+        <h2>Hello world</h2>
         <Navbar />
         <Searchbar />
+
         <Routes>
           <Route path="/about" element={<About />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </div>
     );
