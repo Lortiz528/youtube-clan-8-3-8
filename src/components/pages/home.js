de3a1ea24e82f65adc57f5d97216fbcacafec7ae
named.josie <named.josie@gmail.com>
2022-06-16 23:10:11 -0400

    wrote some code still missing data

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,8 +1,33 @@
+import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
 import Searchbar from './Searchbar';
 import VideoList from './VideoList';
 
 class Home extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       searchBar: "",
+//       videos: [],
+//     }
+//   }
+//   handleSearch = (event) => {
+//     const {value} = event.target;
+//     this.setState({searchBar: value})
+//   }
+// handleSubmit = (event) => {
+//   event.preventDefault();
+//   fetch('data.json')
+
+//   .then((response) => response.json())
+
+//   .then((obj) => {
+//   this.setState({video: [...obj.items]})
+// });
+// this.setState({searchBar: " "})
+// }
+
+
   render() {
     return (
       <div>
